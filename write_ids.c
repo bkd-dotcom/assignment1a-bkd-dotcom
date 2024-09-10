dd3e1d938f01eebe03d08d2549203441cee65ab2
bkd-dotcom <bkd5497@psu.edu>
2024-09-10 00:07:20 -0400

    Your message

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "bkd-dotcom";
+    char STUDENT_ID[] = "903801484";
 
     return 0 ;
 }
